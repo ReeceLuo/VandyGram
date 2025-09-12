3bdd6088d5ee0f42707a59242cff1f47bf7db56a
Reece Luo <reeceluo101@gmail.com>
2025-09-11 22:05:20 -0500

    Added serve import

--- server/server.js
+++ server/server.js
@@ -2,19 +2,19 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/db.js";
-import {inngest, functions} from "./inngest/index.js";
+import { inngest, functions } from "./inngest/index.js";
+import { serve } from "inngest/express";
 
 const app = express();
 
-await connectDB();  // whenever server starts, it connects to MongoDB database
+await connectDB(); // whenever server starts, it connects to MongoDB database
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res)=> res.send("Server is running"));
+app.get("/", (req, res) => res.send("Server is running"));
 app.use("/api/ingest", serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, ()=>console.log(`Server is running on port ${PORT}`));
-
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
