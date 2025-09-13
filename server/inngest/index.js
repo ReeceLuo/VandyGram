e29747886e945dcc07f918be0f96628ebe29bc63
Reece Luo <reeceluo101@gmail.com>
2025-09-13 10:34:51 -0500

    Checking connection at inngest functions

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import User from "../models/User.js";
 import connectDB from "../configs/db.js";
+import mongoose from "mongoose";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "my-app" });
@@ -12,6 +13,8 @@ const syncUserCreation = inngest.createFunction(
   { id: "sync-user-from-clerk" },
   { event: "clerk/user.created" },
   async ({ event }) => {
+    console.log("Mongoose readyState:", mongoose.connection.readyState); // 1 = connected, 0 = disconnected
+
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
     let username = email_addresses[0].email_address.split("@")[0];
@@ -41,6 +44,8 @@ const syncUserUpdate = inngest.createFunction(
   { id: "update-user-from-clerk" },
   { event: "clerk/user.updated" },
   async ({ event }) => {
+    await connectDB();
+
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
 
@@ -58,6 +63,8 @@ const syncUserDeletion = inngest.createFunction(
   { id: "delete-user-with-clerk" },
   { event: "clerk/user.deleted" },
   async ({ event }) => {
+    await connectDB();
+
     const { id } = event.data;
     await User.findByIdAndDelete(id);
   }
