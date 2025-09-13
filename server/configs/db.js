dcf0a62efd8d813c2d402c8ac593bcedb2b8c142
Reece Luo <reeceluo101@gmail.com>
2025-09-13 06:54:57 -0500

    Revert "Testing for mongoDB"
    This reverts commit 5d7cda93d13f40d70cccd71e65b92419cf6c54d5.

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,14 +3,12 @@ import mongoose from "mongoose";
 // configuration with MongoDB databse
 
 const connectDB = async () => {
-  try {
-    mongoose.connection.on("connected", () =>
-      console.log("Database connected")
-    );
-    await mongoose.connect(`${process.env.MONGODB_URL}/vandygram`);
-  } catch (error) {
-    console.log(error.message);
-  }
-};
+    try {
+        mongoose.connection.on("connected", () => console.log("Database connected"))
+        await mongoose.connect(`${process.env.MONGODB_URL}/VandyGram`)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
 
 export default connectDB;
