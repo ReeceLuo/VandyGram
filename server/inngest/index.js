427c23b35a77e9607849cea0fcb9f1feafde21da
Reece Luo <reeceluo101@gmail.com>
2025-09-13 12:11:21 -0500

    Troubleshooting

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -11,8 +11,10 @@ const syncUserCreation = inngest.createFunction(
   { id: "sync-user-from-clerk" },
   { event: "clerk/user.created" },
   async ({ event }) => {
-    await connectDB(); // Ensure DB connection for this handler
-    const User = (await import("../models/User.js")).default; // Import model after connection
+    mongoose.connection.on("connected", () => {
+      console.log("Database connected");
+    });
+    await mongoose.connect(`${process.env.MONGODB_URL}/VandyGram`);
 
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
@@ -43,9 +45,6 @@ const syncUserUpdate = inngest.createFunction(
   { id: "update-user-from-clerk" },
   { event: "clerk/user.updated" },
   async ({ event }) => {
-    await connectDB(); // Ensure DB connection for this handler
-    const User = (await import("../models/User.js")).default; // Import model after connection
-
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
 
@@ -63,9 +62,6 @@ const syncUserDeletion = inngest.createFunction(
   { id: "delete-user-with-clerk" },
   { event: "clerk/user.deleted" },
   async ({ event }) => {
-    await connectDB(); // Ensure DB connection for this handler
-    const User = (await import("../models/User.js")).default; // Import model after connection
-
     const { id } = event.data;
     await User.findByIdAndDelete(id);
   }
